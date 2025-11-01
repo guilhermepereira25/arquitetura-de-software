c6b67a635e5b9b4dc1a05d0c0e481f7a0665f8a2
Guilherme Pereira <guilhermepereiradv@gmail.com>
2025-11-01 13:36:21 -0300

    add initial Apollo Server setup with dotenv configuration

--- api/graphql/server.ts
+++ api/graphql/server.ts
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { typeDefs } from './schema';
@@ -12,4 +14,6 @@ const { url} = await startStandaloneServer(server, {
   listen: { port: 4000 },
 }); 
 
-console.log(`🚀  Server ready at: ${url}`);
+console.log("Is using Prisma:", process.env.USE_PRISMA);
+
+console.log(`🚀 Server ready at: ${url}`);
